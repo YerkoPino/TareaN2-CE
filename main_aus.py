af8032ffda431df046a3bddafc988a791d0101b8
YerkoPino <yerko.pino@usach.cl>
2016-06-28 17:04:13 -0400

    Correccion de funciones y parametros

--- main_aus.py
+++ main_aus.py
@@ -1,29 +1,27 @@
-#AUS
+#AUS 
 
 from pyevolve import *
 import math
 import random
 
 error_accum = Util.ErrorAccumulator()
-fitness = 0
+fitness = -1
+best_const = 0
 
-# This is the functions used by the GP core,
-# Pyevolve will automatically detect them
-# and the they number of arguments
+#Funciones
 def gp_add(a, b): return a+b
 def gp_sub(a, b): return a-b
 def gp_mul(a, b): return a*b
-#def gp_sqrt(a):   return math.sqrt(abs(a))
 def gp_div(a, b): 
    if b==0:
-      return a
+      return a/math.pow(10,-50)
    else:
       return a/b
-#def gp_exp(a): return math.exp(a)
-#def gp_leq(a, b): return a<=b
-#def gp_geq(a, b): return a>=b
-#def gp_and(a, b): return a and b
-#def gp_or(a, b): return a or b
+def gp_exp(a):
+   if a<100:
+      return math.exp(a)
+   else:
+      return math.exp(100)
 def gp_if_geq_else(a, b, c, d):
    if a>=b:
       return c
@@ -35,9 +33,11 @@ def gp_if_leq_else(a, b, c, d):
    else:
       return d
 
+#Fitness
 def eval_func(chromosome):
    global error_accum
    global fitness
+   global best_const
    error_accum.reset()
    code_comp = chromosome.getCompiledCode()
 
@@ -69,51 +69,44 @@ def eval_func(chromosome):
 
    archivo.close()
 
-   if fitness==0 or error_accum.getRMSE()<=fitness:
+   if fitness==-1 or error_accum.getRMSE()<=fitness:
       fitness = error_accum.getRMSE()
+      best_const = const
 
    return error_accum.getRMSE()
 
-   #for a in xrange(0, 5):
-   #   for b in xrange(0, 5):
-         # The eval will execute a pre-compiled syntax tree
-         # as a Python expression, and will automatically use
-         # the "a" and "b" variables (the terminals defined)
-   #      evaluated     = eval(code_comp)
-   #      target        = math.sqrt((a*a)+(b*b))
-   #      error_accum += (target, evaluated)
-   #return error_accum.getRMSE()
-
 def step_callback(gp_engine):
-   if gp_engine.getCurrentGeneration() == 4:
-      GTree.GTreeGP.writePopulationDot(gp_engine, "trees.jpg", start=0, end=1)
+   if gp_engine.getCurrentGeneration() == 49:
+      GTree.GTreeGP.writePopulationDot(gp_engine, "trees_aus.jpg", start=0, end=1)
 
 def main_run():
    global fitness
+   global best_const
    genome = GTree.GTreeGP()
    genome.setParams(max_depth=5, method="ramped")
    genome.evaluator.set(eval_func)
 
    ga = GSimpleGA.GSimpleGA(genome)
    ga.stepCallback.set(step_callback)
-   # This method will catch and use every function that
-   # begins with "gp", but you can also add them manually.
-   # The terminals are Python variables, you can use the
-   # ephemeral random consts too, using ephemeral:random.randint(0,2)
-   # for example.
-   ga.setParams(gp_terminals       = ['const', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9', 'c10', 'c11', 'c12', 'c13', 'c14'],
+   sqlite_adapter = DBAdapters.DBSQLite(identify="exaus")
+   ga.setDBAdapter(sqlite_adapter)
+
+   ga.setParams(gp_terminals       = [ 'const', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9', 'c10',
+                                       'c11', 'c12', 'c13', 'c14'],
                 gp_function_prefix = "gp")
-   # You can even use a function call as terminal, like "func()"
-   # and Pyevolve will use the result of the call as terminal
+
    ga.setMinimax(Consts.minimaxType["minimize"])
-   ga.setGenerations(5)
+   ga.setGenerations(50)
    ga.setMutationRate(0.08)
    ga.setCrossoverRate(1.0)
-   ga.setPopulationSize(500)
+   ga.setPopulationSize(1000)
    ga.evolve(freq_stats=5)
 
    print ga.bestIndividual()
+   print 'Fitness mejor individuo'
    print fitness
+   print 'Valor constante'
+   print best_const
 
 if __name__ == "__main__":
    main_run()
